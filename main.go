4a201e97d97057f7707dc569a1b015e74739159f
Felix Andrew Sapalaran <>
2021-01-28 15:03:01 -0500

    adding contact template

--- main.go
+++ main.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate *template.Template
+	// added contact template container
+	contactTemplate *template.Template
+)
 
 func main() {
-	// declare err here
 	var err error
-	// home.gohtml is the template we are using to render our page
-	// this way it assumes both variables homeTemplate and err are already declared
-	// using this approach prevents scoping problem especially for "homeTemplate"
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
-	// check if our template load properly
 	if err != nil {
-		// if error occurs for loading our template panic the error
-		// the program will stops here if there's a problem
 		panic(err)
 	}
-	// This server wont start if there's problem loading the template
+	// parsing contact template file
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
 
-// home handler
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	// updated our function to use our template
-	// by executing the homeTemplate
-	if err := homeTemplate.Execute(w, nil); err != nil { // wrapping it using "if err equal type" thing
-		// if there an error executing the template then panic the error
+	err := homeTemplate.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
-// contact handler
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to "+
-		"<a href=\"mailto:support@usegolang.com\">support@usegolang.com</a>.")
+	// Execute contact template
+	err := contactTemplate.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
