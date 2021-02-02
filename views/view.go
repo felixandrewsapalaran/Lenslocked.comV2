3c423f72487d65823459d3d57e2f7e9ccad99096
Felix Andrew Sapalaran <>
2021-02-01 19:28:37 -0500

    Created a view type for rendering templates

---
+++ views/view.go
@@ -0,0 +1,26 @@
+package view
+
+/*This file will handle our view logic*/
+
+import "html/template"
+
+// NewView parse all the files necessary and return them to us
+func NewView(files ...string) *View {
+	files = append(files, "views/layouts/footer.gohtml")
+
+	// Parse the files
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	// return pointer to a view
+	return &View{
+		Template: t,
+	}
+}
+
+// View contains our logic template
+type View struct {
+	Template *template.Template
+}
