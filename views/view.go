0422c86e3ca20a16b0d389129489fce0a77717a9
Felix Andrew Sapalaran <>
2021-02-05 12:10:46 -0500

    Cleaning up our code by globbing template files

--- views/view.go
+++ views/view.go
@@ -1,23 +1,28 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	// LayoutDir is the directory where we store our layouts in
+	LayoutDir string = "views/layouts/"
+	// TemplateExt is the extension of our files
+	TemplateExt string = ".gohtml"
+)
 
 // NewView parse all the files necessary and return them to us
 func NewView(layout string, files ...string) *View {
+	// calling this function layoutFiles() which gives us a slice
+	// but append takes a variadic parameter (...) as a last argument
+	files = append(files, layoutFiles()...)
 
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/navbar.gohtml", // add navbar template
-		"views/layouts/bootstrap.gohtml",
-	)
-
-	// which ever `yield` pass in is the one to get use
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
 
-	// return pointer to a view
 	return &View{
 		Template: t,
 		Layout:   layout,
@@ -27,6 +32,19 @@ func NewView(layout string, files ...string) *View {
 // View contains our logic template
 type View struct {
 	Template *template.Template
-	// this Layout is going to store the layout we want to render
-	Layout string
+	Layout   string
+}
+
+// layoutFiles returns a slice of strings representing
+// the layout files used in our application.
+func layoutFiles() []string {
+	// REMOVED
+	// files, err := filepath.Glob("views/layouts/*.gohtml")
+
+	// REPLACED
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
